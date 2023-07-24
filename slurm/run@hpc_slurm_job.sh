07f9184459448f10db46fcfd2a99eb6672fbec38
Eduardo Ribeiro <1600820@estudante.uab.pt>
2023-07-24 19:42:18 +0100

    Updated Makefile and slurm files for new clusted (cirrus.8)

--- slurm/run@hpc_slurm_job.sh
+++ slurm/run@hpc_slurm_job.sh
@@ -2,10 +2,11 @@
 
 ##CHANGE THIS!
 
-#SBATCH --job-name="lines-bench_dataset@hpc"
-#SBATCH --time=0:10:0
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --job-name="P3-16-bench_dataset@hpc"
+##SBATCH --time=0:10:0
+##SBATCH --nodes=1
+##SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=16
 
 ## The operation changes the dataset file used so we need to make a copy
 DATASET_FILE="../../datasets/bench_dataset.hd5.original"
@@ -15,28 +16,30 @@ DATASET_NAME="dados"
 
 EXE="../bin/laid-hdf5-mpi-lines"
 
+## DON'T CHANGE THIS!
+
 # Used to guarantee that the environment does not have any other loaded module
 module purge
 
 # Load software modules. Please check session software for the details
-module load hdf5/1.12.0
-##module load gcc83/openmpi/4.1.1
-module load clang/openmpi/4.0.3
+module load gcc11/libs/hdf5/1.14.0
 
-## DON'T CHANGE THIS!
+# Disable warning for mismatched library versions
+# Cirrus.8 has different hdf5 versions on short and hpc partitions
+# even if we load the same module
+# ##Headers are 1.14.0, library is 1.10.5
+HDF5_DISABLE_VERSION_CHECK=2
+export HDF5_DISABLE_VERSION_CHECK
 
-# Be sure to request the correct partition to avoid the job to be held in the queue, furthermore
-#	on CIRRUS-B (Minho)  choose for example HPC_4_Days
-#	on CIRRUS-A (Lisbon) choose for example hpc
-#SBATCH --partition=hpc
 
 # Run
-echo "=== Copy dataset ==="
 if [ -f "$DATASET_FILE" ]; then
-    echo "=== Running ==="
     if [ -f "$EXE" ]; then
+        echo "=== Running ==="
         chmod u+x $EXE
-        mpiexec --display-map -np $SLURM_NTASKS $EXE -d $DATASET_NAME -f $DATASET_FILE
+        mpiexec -np $SLURM_NTASKS $EXE -d $DATASET_NAME -f $DATASET_FILE
+    else
+        echo "$EXE Not found!"
     fi
 else
     echo "Input dataset not found! [$DATASET_FILE]"
